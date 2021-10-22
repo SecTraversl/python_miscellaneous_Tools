9703078a2a4e153b92584086da2a406fc6d4a698
Travis Logue <SecTraversl@ProtonMail.com>
2021-10-22 08:21:52 -0700

    Last write time 2021-10-22

---
+++ oneliner_ifelse_demo.py
@@ -0,0 +1,24 @@
+# %%
+#######################################
+def oneliner_ifelse_demo():
+    """This code demonstrates how to successfully implement a one-liner if/else statement
+
+    Examples:
+        >>> mystring = 'Complex is better than complicated.'\n
+        >>> secondstring = ' blah '\n
+        >>> bigger_string = mystring if mystring > secondstring else secondstring\n
+        >>> bigger_string\n
+        'Complex is better than complicated.'
+
+    References:
+        One liner if/else: https://youtu.be/MHlwl6GsT8s?t=990
+    """
+    demo_string = """
+    >>> mystring = 'Complex is better than complicated.'
+    >>> secondstring = ' blah '
+    >>> bigger_string = mystring if mystring > secondstring else secondstring
+    >>> bigger_string
+    'Complex is better than complicated.'
+    """
+    print(demo_string)
+
