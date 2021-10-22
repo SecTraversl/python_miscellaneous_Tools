acff051aff8e98c45db499e51436a3f226658400
Travis Logue <SecTraversl@ProtonMail.com>
2021-10-22 08:20:18 -0700

    Last write time 2021-10-22

---
+++ print_docstring.py
@@ -0,0 +1,14 @@
+# %%
+#######################################
+def print_docstring(obj: object):
+    """Prints the docstring for a given function
+
+    Example:
+        >>> print_docstring(len)
+        Return the number of items in a container.
+
+    References:
+
+    """
+    print(obj.__doc__)
+
